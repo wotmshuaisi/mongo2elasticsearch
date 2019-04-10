cd315d33d731d02b2f3f3e6b14de976954ed124a
Charlie <charlanders1@gmail.com>
2019-04-10 18:22:04 +0800

    added index detection on connection function && decode bson to json

--- main.go
+++ main.go
@@ -3,9 +3,6 @@ package main
 import (
 	"context"
 	"flag"
-	"time"
-
-	"gopkg.in/cheggaaa/pb.v1"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -21,6 +18,43 @@ var (
 	elasticClient *elastic.Client
 	collection    *mongo.Collection
 )
+
+func getClients(eStr, mStr, db, c string) {
+	/* elasticsearch client */
+	ctx := context.Background()
+	elasticClient, err := elastic.NewClient(
+		elastic.SetURL(eStr),
+		elastic.SetSniff(false),
+	)
+	if err != nil {
+		panic(err)
+	}
+	if _, _, err := elasticClient.Ping("http://localhost:9200").Do(ctx); err != nil {
+		panic(err)
+	}
+	if ok, err := elasticClient.IndexExists(c).Do(ctx); err != nil {
+		panic(err)
+	} else {
+		if !ok {
+			res, err := elasticClient.CreateIndex(c).Do(ctx)
+			if err != nil {
+				panic(err)
+			}
+			if res.Acknowledged != true {
+				panic(res.Acknowledged)
+			}
+		}
+	}
+	/* mongodb client */
+	mgoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(mStr))
+	if err != nil {
+		panic(err)
+	}
+	if err := mgoClient.Ping(ctx, nil); err != nil {
+		panic(err)
+	}
+	collection = mgoClient.Database(db).Collection(c)
+}
 
 func main() {
 	/* variable definition */
@@ -42,43 +76,26 @@ func main() {
 	if err != nil {
 		logrus.WithError(err).Fatalln("mongo client count() error")
 	}
-	// cursor, err := collection.Find(context.Background(), bson.M{})
-	// if err != nil {
-	// 	logrus.WithError(err).Fatalln("mongo client find() error")
-	// }
-	// for cursor.Next(ctx) {
-
-	// }
-
-	/* progress bar */
-	bar := pb.StartNew(int(count))
-	for i := int64(0); i < count; i++ {
-		bar.Increment()
-		time.Sleep(time.Millisecond)
-	}
-	bar.FinishPrint("done.")
-}
-
-func getClients(eStr, mStr, db, c string) {
-	/* elasticsearch client */
-	ctx := context.Background()
-	elasticClient, err := elastic.NewClient(
-		elastic.SetURL(eStr),
-		elastic.SetSniff(false),
-	)
+	cursor, err := collection.Find(context.Background(), bson.M{})
 	if err != nil {
-		panic(err)
+		logrus.WithError(err).Fatalln("mongo client find() error")
 	}
-	if _, _, err := elasticClient.Ping("http://localhost:9200").Do(ctx); err != nil {
-		panic(err)
+
+	// progress bar
+	// bar := pb.StartNew(int(count))
+	_ = count
+	for cursor.Next(context.Background()) {
+		itemBytes, err := bson.MarshalExtJSON(cursor.Current, false, true)
+		if err != nil {
+			logrus.WithError(err).Warnln("cursor decode error")
+		}
+		_ = itemBytes
+		break
 	}
-	/* mongodb client */
-	mgoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(mStr))
-	if err != nil {
-		panic(err)
-	}
-	if err := mgoClient.Ping(ctx, nil); err != nil {
-		panic(err)
-	}
-	collection = mgoClient.Database(db).Collection(c)
+
+	// for i := int64(0); i < count; i++ {
+	// 	bar.Increment()
+	// 	time.Sleep(time.Millisecond)
+	// }
+	// bar.FinishPrint("done.")
 }
