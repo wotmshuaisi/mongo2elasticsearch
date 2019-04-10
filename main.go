ac7d0c93b33ffd4d1e344ec44f694d8d93f90633
wotmshuaisi <16408269+wotmshuaisi@users.noreply.github.com>
2019-04-10 17:38:29 +0800

    added prograss bar

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"context"
 	"flag"
+	"time"
 
+	"gopkg.in/cheggaaa/pb.v1"
+
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -19,18 +23,40 @@ var (
 )
 
 func main() {
-	// program parameters
+	/* variable definition */
+	// var dataList = make(chan bson., size)
+	/* program parameters */
 	var mstr = flag.String("mstr", "mongodb://localhost:27017", "mongodb connection string [mongodb://localhost:27017]")
 	var estr = flag.String("estr", "http://localhost:9200", "elasticsearch connection string [http://localhost:9200]")
 	var db = flag.String("db", "", "mongodb database[required]")
 	var c = flag.String("c", "", "mongodb database collection[required]")
 	flag.Parse()
-	if db == nil || c == nil || *db == "" || *c == "" {
-		logrus.Fatalln("database && collection could not be empty")
-	}
-	// init clients
+	// if db == nil || c == nil || *db == "" || *c == "" {
+	// 	logrus.Fatalln("database && collection could not be empty")
+	// }
+	/* init clients */
 	getClients(*estr, *mstr, *db, *c)
+	/* processing */
+	// get document count
+	count, err := collection.CountDocuments(context.Background(), bson.M{})
+	if err != nil {
+		logrus.WithError(err).Fatalln("mongo client count() error")
+	}
+	// cursor, err := collection.Find(context.Background(), bson.M{})
+	// if err != nil {
+	// 	logrus.WithError(err).Fatalln("mongo client find() error")
+	// }
+	// for cursor.Next(ctx) {
 
+	// }
+
+	/* progress bar */
+	bar := pb.StartNew(int(count))
+	for i := int64(0); i < count; i++ {
+		bar.Increment()
+		time.Sleep(time.Millisecond)
+	}
+	bar.FinishPrint("done.")
 }
 
 func getClients(eStr, mStr, db, c string) {
