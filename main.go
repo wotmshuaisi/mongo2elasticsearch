f6e4168e19d265bb2d981438c70e32a32b436dec
Charlie <charlanders1@gmail.com>
2019-04-10 16:56:21 +0800

    achieved elastic client && mongoclient

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"flag"
 
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"go.mongodb.org/mongo-driver/mongo"
+
 	"github.com/sirupsen/logrus"
 
 	"github.com/olivere/elastic"
@@ -11,6 +15,7 @@ import (
 
 var (
 	elasticClient *elastic.Client
+	collection    *mongo.Collection
 )
 
 func main() {
@@ -19,19 +24,20 @@ func main() {
 	var estr = flag.String("estr", "http://localhost:9200", "elasticsearch connection string [http://localhost:9200]")
 	var db = flag.String("db", "", "mongodb database[required]")
 	var c = flag.String("c", "", "mongodb database collection[required]")
+	flag.Parse()
 	if db == nil || c == nil || *db == "" || *c == "" {
 		logrus.Fatalln("database && collection could not be empty")
 	}
-	// init elasticsearch client
-	getElasticSearchClient(*estr)
+	// init clients
+	getClients(*estr, *mstr, *db, *c)
 
 }
 
-func getElasticSearchClient(conStr string) {
+func getClients(eStr, mStr, db, c string) {
 	/* elasticsearch client */
 	ctx := context.Background()
 	elasticClient, err := elastic.NewClient(
-		elastic.SetURL(conStr),
+		elastic.SetURL(eStr),
 		elastic.SetSniff(false),
 	)
 	if err != nil {
@@ -40,4 +46,13 @@ func getElasticSearchClient(conStr string) {
 	if _, _, err := elasticClient.Ping("http://localhost:9200").Do(ctx); err != nil {
 		panic(err)
 	}
+	/* mongodb client */
+	mgoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(mStr))
+	if err != nil {
+		panic(err)
+	}
+	if err := mgoClient.Ping(ctx, nil); err != nil {
+		panic(err)
+	}
+	collection = mgoClient.Database(db).Collection(c)
 }
